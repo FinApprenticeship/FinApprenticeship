1cfe384053651fad3fc7c300e3b5b6fbd5d84fdf
Alexander Bösecke <boesi@users.noreply.github.com>
2025-06-19 11:59:26 +0200

    home with image

--- Dashboard/home.py
+++ Dashboard/home.py
@@ -1,8 +1,13 @@
 import streamlit as st
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
 
 def app():
     with st.container():
-        st.markdown('<div class="custom-logo">', unsafe_allow_html=True)
+        st.markdown('<div class="cstom-logo">', unsafe_allow_html=True)
+        st.image(os.path.join(base_dir, "FinApprenticeship_Background.png"), width=90)
         st.markdown('</div>', unsafe_allow_html=True)
 
     st.markdown("""
