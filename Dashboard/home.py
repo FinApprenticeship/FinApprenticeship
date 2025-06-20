160c3ff29a0cbe24de776e70472be464ae6c410d
Alexander Bösecke <boesi@users.noreply.github.com>
2025-06-20 08:14:05 +0200

    converted images to webp

--- Dashboard/home.py
+++ Dashboard/home.py
@@ -3,7 +3,7 @@ import os
 
 def app():
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    image_path = os.path.join(base_dir, "assets", "startpage.png")
+    image_path = os.path.join(base_dir, "assets", "startpage.webp")
     st.image(image_path, use_container_width=True)
 
 app()
